a5df416b861dce9c124e2593187ecd064d76bb10
sidali <sidalibaiche17@gmail.com>
2020-03-26 08:26:17 +0100

    4er commit

--- client_component_publish.py
+++ client_component_publish.py
@@ -30,7 +30,7 @@ from twisted.internet.defer import inlineCallbacks
 import os
 import argparse
 
-url = os.environ.get('CBURL', 'ws://localhost:8080/ws')
+url = os.environ.get('CBURL', 'ws://172.17.0.2:8080/ws')
 realmv = os.environ.get('CBREALM', 'realm1')
 topic = os.environ.get('CBTOPIC', 'com.myapp.hello')
 print(url, realmv)
